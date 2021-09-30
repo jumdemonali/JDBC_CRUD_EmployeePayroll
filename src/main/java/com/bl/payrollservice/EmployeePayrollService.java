be4df8fdfce4526c4c01bfe27fdfa4ee081d0653
jumdemonali <jumdemonali@gmail.com>
2021-09-30 13:10:57 +0530

    Added: Welcome message

---
+++ src/main/java/com/bl/payrollservice/EmployeePayrollService.java
@@ -0,0 +1,8 @@
+package com.bl.payrollservice;
+
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to database connectivity program!!!");
+    }
+}
+
